f4bd347631e490457e0ba703ea15688b3e3ed06a
kathytong-hashicorp <kathy.tong@hashicorp.com>
2025-01-09 10:39:34 -0500

    Update file.py

--- file.py
+++ file.py
@@ -47,3 +47,4 @@ aws_mws_key = 'amzn.mws.a8fc03d7-7eb3-c92f-b3aa-ae93cbff7ac3'
 aws_mws_key = 'amzn.mws.a8fc03d7-7eb3-c92f-b3aa-ae93cbff7ac1'
 
 password = 'eKDFhwiueyfiuwyer#R!!!3'
+password = 'eKDFhwiueyfiuwyer#R!!!2'
